f02053d6561619f0fc38d05f2fe64c5b46bc97bc
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-02-25 20:19:14 +0200

    API

---
+++ app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+    res.send('Done');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
