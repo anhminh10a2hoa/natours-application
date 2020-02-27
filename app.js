4084b2bd9c7a46182d0e9c2978c654c3d87fe486
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-02-27 05:40:06 +0200

    server

--- app.js
+++ app.js
@@ -16,7 +16,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
