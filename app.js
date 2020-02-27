d6b98cdd91cb694e3afb84d6eaab0d9422f7fd5a
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-02-27 20:58:47 +0200

    Using dotenv

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
 
-const morgan = require('morgan');
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
-app.use((req, res, next) => {
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
