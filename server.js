dbdd814122e4ae63e86070a0bdbf684acc19fb21
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-02-29 23:38:27 +0200

    test data

--- server.js
+++ server.js
@@ -25,14 +25,33 @@ const app = require('./app');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name']
+    required: [true, 'A tour must have a name'],
+    unique: true
   },
-  rating: Number,
+  rating: {
+    type: Number,
+    default: 4.5
+  },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
   }
 });
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
