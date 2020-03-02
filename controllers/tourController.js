26198496dcb5db4d5fb941861f1cb3f19a6a8aa8
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-02 23:53:20 +0200

    edit tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('./../models/tourModel');
 
 exports.checkID = (req, res, next, val) => {
     console.log(`Tour id is: ${val}`);
@@ -28,44 +25,32 @@ exports.checkBody = (req, res, next) => {
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours
-        }
+        // results: tours.length,
+        // data: {
+        //     tours: tours
+        // }
     });
 };
 
 exports.getTour = (req, res) => {
     const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
+    // const tour = tours.find(el => el.id === id);
+    // res.status(200).json({
+    //     status: 'success',
+    //     data: {
+    //         tour
+    //     }
+    // });
 };
 
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({
-            id: newId
-        },
-        req.body
-    );
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour
-                }
-            });
-        }
-    );
+    res.status(200).json({
+        status: 'success',
+        // results: tours.length,
+        // data: {
+        //     tours: tours
+        // }
+    });
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -22,37 +22,6 @@ mongoose
 
 const app = require('./app');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('ERROR: ', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
