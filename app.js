714340a0e7065f7c595e502c0fff3d1f45483780
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-10 01:22:59 +0200

    add middleware for route

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: '404',
+    message: `Can not find ${req.originalURL} on this server!`
+  });
+});
+
 module.exports = app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,105 +1,108 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters']
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
-    },
-    slug: String,
-    duration: {
-      type: Number,
-      required: [true, 'A tour must have a duration']
-    },
-    maxGroupSize: {
-      type: Number,
-      required: [true, 'A tour must have a group size']
-    },
-    difficulty: {
-      type: String,
-      required: [true, 'A tour must have a difficulty'],
-      enum: {
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult'
-      }
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
-    },
-    ratingsQuantity: {
-      type: Number,
-      default: 0
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price']
-    },
-    priceDiscount: {
-      type: Number,
-      validate: {
-        validator: function(val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: 'Discount price ({VALUE}) should be below regular price'
-      }
-    },
-    summary: {
-      type: String,
-      trim: true, //auto cut the empty space of the beginning and the end
-      required: [true, 'A tour must have a description']
-    },
-    description: {
-      type: String,
-      trim: true
-    },
-    imageCover: {
-      type: String,
-      required: [true, 'A tour must have a cover image']
-    },
-    images: [String],
-    createdAt: {
-      type: Date,
-      default: Date.now()
-    },
-    startDates: [Date],
-    secretTour: {
-      type: Boolean,
-      default: false
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters']
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
     }
   },
-  {
-    toJSON: {
-      virtuals: true
-    },
-    toObject: {
-      virtuals: true
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
     }
+  },
+  summary: {
+    type: String,
+    trim: true, //auto cut the empty space of the beginning and the end
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
   }
-);
+}, {
+  toJSON: {
+    virtuals: true
+  },
+  toObject: {
+    virtuals: true
+  }
+});
 
-tourSchema.virtual('durationWeeks').get(function() {
+tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
 // DOCUMENT MIDDLEWARE: run before .save() and .create()
-tourSchema.pre('save', function(next) {
-  this.slug = slugify(this.name, { lower: true });
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true
+  });
   next();
 });
 
 // QUERY MIDDLEWARE
-tourSchema.pre(/^find/, function(next) {
-  this.find({ secretTour: { $ne: true } });
+tourSchema.pre(/^find/, function (next) {
+  this.find({
+    secretTour: {
+      $ne: true
+    }
+  });
   this.start = Date.now();
   next();
 });
@@ -109,13 +112,20 @@ tourSchema.pre(/^find/, function(next) {
 //   next();
 // });
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start()} millisecond`);
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} millisecond`);
+  next();
 });
 
 //AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+tourSchema.pre('aggregrate', function (next) {
+  this.pipeline().unshift({
+    $match: {
+      secretTour: {
+        $ne: true
+      }
+    }
+  });
   next();
 });
 
