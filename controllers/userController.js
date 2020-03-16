b5d4a3bff2121a4dbbaaada078ab7debf76d7b32
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-16 08:43:13 +0200

    signup and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: '500 Internal Server Error',
-        message: 'Can not find user'
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+    const users = await User.find();
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 
 exports.getUser = (req, res) => {
     res.status(500).json({
--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
+    select: false //never show it
   },
   passwordConfirm: {
     type: String,
@@ -46,6 +47,13 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
