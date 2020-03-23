7720edf683f03ecac7c45c8da7086281382c8fc3
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-23 22:18:29 +0200

    active cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,18 @@ const signToken = id => {
 
 const createAndSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookiesOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
+
+    res.cookie('jwt', token, cookiesOptions)
+
+    // Remove password from output
+    user.password = undefined;
+
     res.status(statusCode).json({
         status: 'success',
         token,
