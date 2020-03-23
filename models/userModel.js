135b2e71aa114d55e869590d0317214329fa8924
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-23 20:41:21 +0200

    Deactive user

--- models/userModel.js
+++ models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function (next) {
@@ -62,6 +67,11 @@ userSchema.pre('save', function(next){
 
   this.passwordChangedAt = Date.now() - 1000;
   next();
+})
+
+userSchema.pre(/^find/, function(next){
+  this.find({active : {$ne : false}});
+  next()
 })
 
 userSchema.methods.correctPassword = async function (
