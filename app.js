ebe47bf939297bc9899ccfe1eaf7b6d5a8fd7868
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-24 00:53:40 +0200

    xss, query injection

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSantize = require('express-mongo-santize');
+const xss = require('xss-clean');
 
 const app = express();
 const morgan = require('morgan');
@@ -6,12 +10,31 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
+// Set security for HTTP header
+app.use(helmet());
 
+// Development logging 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60*60*1000,
+  message: 'Too many requests for this IP, please try again in an hour'
+})
+app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data santitization against NoSQL query injection
+app.use(mongoSantize());
+
+// Data santitization against XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
