cbb888e3bf4c59c66e7ff1d95dd5357b36356d9b
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-25 23:19:00 +0200

    populate referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -100,7 +100,12 @@ const tourSchema = new mongoose.Schema({
       day: Number
     }
   ],
-  guides: []
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
 }, {
   toJSON: {
     virtuals: true
@@ -122,12 +127,13 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next){
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises)
+// Embedded MIDDLEWARE
+// tourSchema.pre('save', async function (next){
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises)
 
-  next();
-})
+//   next();
+// })
 
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
@@ -139,6 +145,14 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -createdAt'
+  })
+  next()
+})
 
 // tourSchema.pre('findOne', function(next) {
 //   this.find({ secretTour: { $ne: true } });
