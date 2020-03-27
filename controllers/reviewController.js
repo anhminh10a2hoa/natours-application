fdafb5fc705156860b36d4087bd5aac713ab4cb6
Tao la` Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-03-27 22:57:15 +0200

    Nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,10 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  // Allow nested routes
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
   res.status(200).json({
     status: 'success',
     results: reviews.length,
@@ -13,6 +16,9 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const review = await Review.create(req.body);
 
   res.status(200).json({
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+//POST /tour/2135hhasd/reviews
 
 router
   .route('/')
